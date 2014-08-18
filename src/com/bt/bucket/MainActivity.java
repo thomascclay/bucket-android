da8342063ff452694e475dbe51a681879c987533
Thomas Clay <thomas.clay@cerner.com>
2014-08-17 22:43:02 -0500

    Skeleton app works.

--- src/com/bt/bucket/MainActivity.java
+++ src/com/bt/bucket/MainActivity.java
@@ -3,15 +3,20 @@ package com.bt.bucket;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     Button addPhraseBtn, submitPhrasesBtn;
-    ArrayList<String> phrases = new ArrayList<String>();
+    EditText phrasesText;
+    ListView phrasesListView;
+    ArrayList<String> phrasesArray = new ArrayList<String>();
     ArrayAdapter<String> adapter;
 
     /**
@@ -21,12 +26,37 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-//        adapter = new ArrayAdapter<String>(this, R.)
 
+        addPhraseBtn = (Button) findViewById(R.id.btn_add);
+        submitPhrasesBtn = (Button) findViewById(R.id.btn_submit);
+        phrasesText = (EditText) findViewById(R.id.edt_phrase);
+        phrasesListView = (ListView) findViewById(R.id.lst_phrases);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phrasesArray);
+        phrasesListView.setAdapter(adapter);
+
+        addPhraseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPhrases(view);
+            }
+        });
+
+        phrasesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                phrasesText.setText((String)adapterView.getItemAtPosition(i));
+                phrasesArray.remove(i);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
 
     public void addPhrases(View v) {
-//        listItems.add()
+        phrasesArray.add(phrasesText.getText().toString());
+        phrasesText.getText().clear();
+        adapter.notifyDataSetChanged();
     }
+
 }
